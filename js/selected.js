ae39dfa6fc92060a1d53d74e55c16760e7d4b852
ForhadRobe <i.rforhad@gmail.com>
2022-08-23 21:35:42 +0600

    utilities created

---
+++ js/selected.js
@@ -0,0 +1,224 @@
+document.getElementById('button-field1').addEventListener('click', function () {
+    //length check
+    const selectedPlayerlength = document.querySelectorAll('#selected-player li');
+    if (selectedPlayerlength.length < 5) {
+        // disabled btn
+        const btnDesabaled = document.getElementById('button-field1');
+        btnDesabaled.setAttribute('disabled', true);
+        btnDesabaled.style.backgroundColor = 'gray';
+
+        //playerName get
+        const name = document.getElementById('name-field1');
+        const elemet = name.innerText;
+        //add hobe
+        const selectedPlayer = document.getElementById('selected-player');
+        const createElement = document.createElement('li');
+        createElement.innerText = elemet;
+
+        selectedPlayer.appendChild(createElement);
+
+    } else {
+        alert('No more option on select button.FIve players already selected by user...');
+    }
+
+});
+document.getElementById('button-field2').addEventListener('click', function () {
+    //length check
+    const selectedPlayerlength = document.querySelectorAll('#selected-player li');
+    if (selectedPlayerlength.length < 5) {
+        // disabled btn
+        const btnDesabaled = document.getElementById('button-field2');
+        btnDesabaled.setAttribute('disabled', true);
+        btnDesabaled.style.backgroundColor = 'gray';
+        //player Name get
+        const name = document.getElementById('name-field2');
+        const elemet = name.innerText;
+
+        // add hobe
+        const selectedPlayer = document.getElementById('selected-player');
+        const createElement = document.createElement('li');
+        createElement.innerText = elemet;
+
+        selectedPlayer.appendChild(createElement);
+
+    } else {
+        alert('No more option on select button.FIve players already selected by user...');
+    }
+
+});
+
+document.getElementById('button-field3').addEventListener('click', function () {
+
+    //length check
+    const selectedPlayerlength = document.querySelectorAll('#selected-player li');
+    if (selectedPlayerlength.length < 5) {
+        // disabled btn
+        const btnDesabaled = document.getElementById('button-field3');
+        btnDesabaled.setAttribute('disabled', true);
+        btnDesabaled.style.backgroundColor = 'gray';
+        //player name get
+        const name = document.getElementById('name-field3');
+        const elemet = name.innerText;
+
+        //add hobe
+        const selectedPlayer = document.getElementById('selected-player');
+        const createElement = document.createElement('li');
+        createElement.innerText = elemet;
+
+        selectedPlayer.appendChild(createElement);
+
+    } else {
+        alert('No more option on select button.FIve players already selected by user...');
+    }
+
+});
+document.getElementById('button-field4').addEventListener('click', function () {
+    //length check
+    const selectedPlayerlength = document.querySelectorAll('#selected-player li');
+    if (selectedPlayerlength.length < 5) {
+        // disabled btn
+        const btnDesabaled = document.getElementById('button-field4');
+        btnDesabaled.setAttribute('disabled', true);
+        btnDesabaled.style.backgroundColor = 'gray';
+        //player name get
+        const name = document.getElementById('name-field4');
+        const elemet = name.innerText;
+        //add hobe 
+        const selectedPlayer = document.getElementById('selected-player');
+        const createElement = document.createElement('li');
+        createElement.innerText = elemet;
+
+        selectedPlayer.appendChild(createElement);
+
+
+    } else {
+        alert('No more option on select button.FIve players already selected by user...');
+    }
+
+});
+
+document.getElementById('button-field5').addEventListener('click', function () {
+    //length check
+    const selectedPlayerlength = document.querySelectorAll('#selected-player li');
+    if (selectedPlayerlength.length < 5) {
+        // disabled btn
+        const btnDesabaled = document.getElementById('button-field5');
+        btnDesabaled.setAttribute('disabled', true);
+        btnDesabaled.style.backgroundColor = 'gray';
+        //player name get
+        const name = document.getElementById('name-field5');
+        const elemet = name.innerText;
+
+        //add hobe
+        const selectedPlayer = document.getElementById('selected-player');
+        const createElement = document.createElement('li');
+        createElement.innerText = elemet;
+
+        selectedPlayer.appendChild(createElement);
+
+    } else {
+        alert('No more option on select button.FIve players already selected by user...');
+    }
+
+});
+
+document.getElementById('button-field6').addEventListener('click', function () {
+    //length check
+    const selectedPlayerlength = document.querySelectorAll('#selected-player li');
+    if (selectedPlayerlength.length < 5) {
+        // disabled btn
+        const btnDesabaled = document.getElementById('button-field6');
+        btnDesabaled.setAttribute('disabled', true);
+        btnDesabaled.style.backgroundColor = 'gray';
+        //player name get
+        const name = document.getElementById('name-field6');
+        const elemet = name.innerText;
+
+        //add hobe
+        const selectedPlayer = document.getElementById('selected-player');
+        const createElement = document.createElement('li');
+        createElement.innerText = elemet;
+
+        selectedPlayer.appendChild(createElement);
+
+    } else {
+        alert('No more option on select button.FIve players already selected by user...');
+    }
+});
+
+document.getElementById('button-field7').addEventListener('click', function () {
+    //length check
+    const selectedPlayerlength = document.querySelectorAll('#selected-player li');
+    if (selectedPlayerlength.length < 5) {
+
+        // disabled btn
+        const btnDesabaled = document.getElementById('button-field7');
+        btnDesabaled.setAttribute('disabled', true);
+        btnDesabaled.style.backgroundColor = 'gray';
+
+        //player name get
+        const name = document.getElementById('name-field7');
+        const elemet = name.innerText;
+
+        //add hobe
+        const selectedPlayer = document.getElementById('selected-player');
+        const createElement = document.createElement('li');
+        createElement.innerText = elemet;
+
+        selectedPlayer.appendChild(createElement);
+
+    } else {
+        alert('No more option on select button.FIve players already selected by user...');
+    }
+});
+
+
+document.getElementById('button-field8').addEventListener('click', function () {
+    //length check
+    const selectedPlayerlength = document.querySelectorAll('#selected-player li');
+    if (selectedPlayerlength.length < 5) {
+
+        // disabled btn
+        const btnDesabaled = document.getElementById('button-field8');
+        btnDesabaled.setAttribute('disabled', true);
+        btnDesabaled.style.backgroundColor = 'gray';
+        //player name get
+        const name = document.getElementById('name-field8');
+        const elemet = name.innerText;
+
+        //add hobe
+        const selectedPlayer = document.getElementById('selected-player');
+        const createElement = document.createElement('li');
+        createElement.innerText = elemet;
+
+        selectedPlayer.appendChild(createElement);
+
+    } else {
+        alert('No more option on select button.FIve players already selected by user...');
+    }
+});
+
+document.getElementById('button-field9').addEventListener('click', function () {
+    //length check
+    const selectedPlayerlength = document.querySelectorAll('#selected-player li');
+    if (selectedPlayerlength.length < 5) {
+
+        // disabled btn
+        const btnDesabaled = document.getElementById('button-field9');
+        btnDesabaled.setAttribute('disabled', true);
+        btnDesabaled.style.backgroundColor = 'gray';
+        //player name get
+        const name = document.getElementById('name-field9');
+        const elemet = name.innerText;
+
+        //add hobe
+        const selectedPlayer = document.getElementById('selected-player');
+        const createElement = document.createElement('li');
+        createElement.innerText = elemet;
+
+        selectedPlayer.appendChild(createElement);
+
+    } else {
+        alert('No more option on select button.FIve players already selected by user...');
+    }
+});
